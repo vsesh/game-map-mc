40d96f3e953d93b71f4de6e09ff8c86c57d61da4
vsesh <v.shmyroff@ya.ru>
2014-11-29 15:49:47 +0300

    part4

---
+++ part3.5/data/placesData.js
@@ -0,0 +1,26 @@
+var placesData = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature", 
+            "id": 0, 
+            "geometry": {"type": "Point", "coordinates": [-0.3, 0]}, 
+            "properties": {"balloonContent": "Home", "hintContent": "Home"}, 
+            "options": {"preset": "game#homeIcon"}
+        },
+        {
+            "type": "Feature", 
+            "id": 1, 
+            "geometry":  {"type": "Point", "coordinates": [0, 0]}, 
+            "properties": {"balloonContent": "Health", "hintContent": "Health"}, 
+            "options": {"preset": "game#healthIcon"}
+        },
+        {
+            "type": "Feature", 
+            "id": 2, 
+            "geometry": {"type": "Point", "coordinates": [0.3, 0]}, 
+            "properties": {"balloonContent": "Jump", "hintContent": "Jump"}, 
+            "options": {"preset": "game#jumpIcon"}
+        }
+    ]
+}
